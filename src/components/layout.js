93ed2bab864034ad94e6a38868fe733343916806
Pablo Cianes <cianespablo@gmail.com>
2020-08-07 13:35:19 +0200

    🎉Clean Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,84 +3,14 @@ import { Link } from 'gatsby'
 
 const Layout = props => {
   const { title, children } = props
-  const [toggleNav, setToggleNav] = React.useState(false)
   return (
-    <div className={`site-wrapper ${toggleNav ? `site-head-open` : ``}`}>
+    <div className="site-wrapper">
       <header className="site-head">
         <div id="menu" className="site-head-container">
-          <a
-            className="nav-burger"
-            href={`#menu`}
-            onClick={() => setToggleNav(!toggleNav)}
-          >
-            <div
-              className="hamburger hamburger--collapse"
-              aria-label="Menu"
-              role="button"
-              aria-controls="navigation"
-            >
-              <div className="hamburger-box">
-                <div className="hamburger-inner" />
-              </div>{' '}
-              <div className="hamburger-text-menu-text hidden">Menu</div>
-            </div>
-          </a>
-          <nav id="swup" className="site-head-left">
-            <ul className="nav" role="menu">
-              {/* <li className="nav-home nav-current" role="menuitem">
-                <Link to={`/`}>Home</Link>
-              </li> */}
-              <li className="nav-about" role="menuitem">
-                <Link to={`/about`}>About</Link>
-              </li>
-              <li className="nav-elements" role="menuitem">
-                <Link to={`/elements`}>Elements</Link>
-              </li>
-              <li className="nav-tags" role="menuitem">
-                <Link to={`/tags`}>Tags</Link>
-              </li>
-            </ul>
-          </nav>
           <div className="site-head-center">
             <Link className="site-head-logo" to={`/`}>
               {title}
             </Link>
-          </div>
-          <div className="site-head-right">
-            <div className="social-links">
-              <a
-                href="https://www.facebook.com"
-                title="Facebook"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Facebook
-              </a>
-              <a
-                href="https://twitter.com"
-                title="Twitter"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Twitter
-              </a>
-              <Link
-                to={`/rss.xml`}
-                title="RSS"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                RSS
-              </Link>
-              <a
-                href="https://github.com/jooplaan/gatsby-london-night-and-day"
-                title="GitHub"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </div>
           </div>
         </div>
       </header>
@@ -90,10 +20,7 @@ const Layout = props => {
         </div>
       </main>
       <footer className="site-foot">
-        &copy; {new Date().getFullYear()} <Link to={`/`}>{title}</Link> &mdash;
-        <Link to="/aviso" rel="noopener noreferrer">
-          Aviso legal
-        </Link>
+        &copy; {new Date().getFullYear()} <Link to={`/`}>{title}</Link>
       </footer>
     </div>
   )
